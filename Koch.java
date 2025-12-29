5262f8b389f74d8bc08009e6aef6a471e6af83c7
Ariel Yatskan <ariel.yatskan@post.runi.ac.il>
2025-12-29 17:16:35 +0200

    Done

--- Koch.java
+++ Koch.java
@@ -17,18 +17,37 @@ public class Koch {
 		      Double.parseDouble(args[3]), Double.parseDouble(args[4]));
 		*/
 
+
 		/*
 		// Tests the snowflake function:
 		// Gets n, and draws a Koch snowflake of n edges in the standard canvass.
+		/* */
 		snowFlake(Integer.parseInt(args[0]));
-		*/
+		
 	}
 
 	/** Gets n, x1, y1, x2, y2,
      *  and draws a Koch curve of depth n from (x1,y1) to (x2,y2). */
 	public static void curve(int n, double x1, double y1, double x2, double y2) {
-		StdDraw.line(x1, y1, x2, y2);
-		//// Write the rest of your code below.
+		if(n == 0)  System.out.println("Enter a valid number n");
+		double xP3 = ((Math.sqrt(3.0) / 6) * (y1 - y2)) + ((x1 + x2) / 2);
+		double yP3 = ((Math.sqrt(3.0) / 6) * (x2 - x1)) + ((y1 + y2) / 2);
+		double newX1 = ((x2 - x1) / 3) + x1, newY1 = ((y2 - y1) / 3) + y1;
+		double newX2 = (((x2 - x1) / 3) * 2) + x1, newY2 = (((y2 - y1) / 3) * 2) + y1;
+		if(n == 1) {
+			StdDraw.line(x1, y1, x2, y2);
+			StdDraw.setPenColor(StdDraw.WHITE);
+			StdDraw.line(newX1, newY1, newX2, newY2);
+			StdDraw.setPenColor(StdDraw.BLACK);
+			StdDraw.line(newX1, newY1, xP3, yP3);
+			StdDraw.line(xP3, yP3, newX2, newY2);
+		}
+		else {
+			curve(n - 1, x1, y1, newX1, newY1);
+			curve(n - 1, newX1, newY1, xP3, yP3);
+			curve(n - 1, xP3, yP3, newX2, newY2);
+			curve(n - 1, newX2, newY2, x2, y2);
+		}
 	}
 
     /** Gets n, and draws a Koch snowflake of n edges in the standard canvass. */
@@ -37,6 +56,11 @@ public class Koch {
 		StdDraw.setYscale(0, 1.1);
 		StdDraw.setXscale(0, 1.1);
 		// Draws a Koch snowflake of depth n
-		//// Write the rest of your code below.
+		double x1 = 0.2, y1 = 0.6;
+		double x2 = 0.6, y2 = 0.6;
+		double x3 = 0.4, y3 = 1 - (0.4 * Math.sqrt(3.0));
+		curve(n, x1, y1, x2, y2);
+		curve(n, x2, y2, x3, y3);
+		curve(n, x3, y3, x1, y1);
 	}
 }
